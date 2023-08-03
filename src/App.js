a9200063666c3f722401acfceef2bf27a77f6cd8
donell <donell.wambua@student.moringaschool.com>
2023-08-03 14:17:04 +0300

    added footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import './App.css';
 import Homepage from "./components/Homepage";
-
+import Filterbar from './components/Filterbar';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -28,11 +29,22 @@ function App() {
   return (
     <div className="App">
       <h1>Hi</h1>
+          <div>
+            
+            <Filterbar />
+
+           
+          </div>
+          
           <div className='biggerDiv'>
           {eachCar}
           </div>
+          <Footer/>
+          
     </div>
   );
+
+
 }
 
 export default App;
